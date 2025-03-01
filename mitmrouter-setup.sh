1c3464e2c39dc3111108d94b32ce3d2f1b1c82a1
Adam Sage <30732752+asage-me@users.noreply.github.com>
2025-03-01 11:53:44 -0500

    Update mitmrouter-setup.sh

--- mitmrouter-setup.sh
+++ mitmrouter-setup.sh
@@ -4,6 +4,6 @@ sudo apt install -y nano hostapd bridge-utils iptables net-tools
 sudo sysctl -w net.ipv4.ip_forward=1
 git clone https://github.com/nmatt0/mitmrouter.git
 sed -i 's/WAN_IFACE="eth0"/WAN_IFACE="ens2f0"/g' mitmrouter/mitmrouter.sh
-sed -i 's/LAN_IFACE="eth1"/WAN_IFACE="ens2f0"/g' mitmrouter/mitmrouter.sh
+sed -i 's/LAN_IFACE="eth1"/LAN_IFACE="ens2f1"/g' mitmrouter/mitmrouter.sh
 sed -i 's/WIFI_IFACE="wlan0"/WIFI_IFACE="wlp4s0"/g' mitmrouter/mitmrouter.sh
 chmod +x ~/mitmrouter/mitmrouter.sh
