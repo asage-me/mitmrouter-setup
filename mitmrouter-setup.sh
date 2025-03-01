525fe767fd48e6ba3dfacd4d091ced3a96b22ddf
Adam Sage <30732752+asage-me@users.noreply.github.com>
2025-03-01 11:58:06 -0500

    Update mitmrouter-setup.sh

--- mitmrouter-setup.sh
+++ mitmrouter-setup.sh
@@ -2,6 +2,9 @@ sudo apt update
 sudo apt -y full-upgrade
 sudo apt install -y nano hostapd bridge-utils iptables net-tools dnsmasq
 sudo sysctl -w net.ipv4.ip_forward=1
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+sudo systemctl mask systemd-resolved
 git clone https://github.com/nmatt0/mitmrouter.git
 sed -i 's/WAN_IFACE="eth0"/WAN_IFACE="ens2f0"/g' mitmrouter/mitmrouter.sh
 sed -i 's/LAN_IFACE="eth1"/LAN_IFACE="ens2f1"/g' mitmrouter/mitmrouter.sh
